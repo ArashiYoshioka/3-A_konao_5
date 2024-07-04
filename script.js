9764f2a12b5ad6a7dd1ba463a52246cfcbefff47
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 04:43:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    const answerInput = document.getElementById('answerInput').value;
+    const result = document.getElementById('result');
+
+    if (answerInput === "ちょうききゅうか") {
+        result.textContent = "正解！";
+    } else {
+        result.textContent = "残念...";
+    }
+}
